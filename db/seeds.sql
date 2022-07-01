aee4167327d7e8f57b91c63dfbf2d84a2639db66
Villager <deiondrae@gmail.com>
2022-07-01 17:04:38 -0400

    Added seeds to seeds.sql, added connection to database

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO departments(name)
+VALUES
+    ("Sales"),
+    ("Engineering"),
+    ("Finance"),
+    ("Legal"),
+    ("Free Labour")
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ("Salesperson", 80000, 1),
+    ("Lead Engineer", 150000, 2),
+    ("Software Engineer", 120000, 2),
+    ("Account Manager", 160000, 3),
+    ("Accountant", 125000, 3),
+    ("Legal Team Lead", 250000, 4),
+    ("Lawyer", 190000, 4),
+    ("Bootcamp Student", 0, 5)
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ("John", "Doe", 8, NULL),
+    ("Mike", "Chan", 1, 1),
+    ("Ashley", "Rodriguez", 2, NULL),
+    ("Kevin", "Tupik", 3, 3),
+    ("Kunal", "Singh", 4, NULL),
+    ("Malia", "Brown", 5, 5)
+    ("Sarah", "Lourd", 6, NULL),
+    ("Tom", "Allen", 7, 7),
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-//import express
-const express = require("express");
+const inquirer = require("inquirer");
+const consoleT = require("console.table");
 //import connection.js
 const db = require("./db/connection");
+
 //port designation for server.js
 const PORT = process.env.PORT || 3001;
-const app = express();
-app.use(express.urlencoded)
+
+db.connect(err => {
+    if(err) throw err;
+    console.log("Database Connected.");
+});
